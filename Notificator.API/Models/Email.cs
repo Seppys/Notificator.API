091e6d291c27ac051d0e2eefe292c04b24bc7f6c
Jesus Ramirez <jesusmanuelr018@gmail.com>
2023-10-11 05:59:01 +0200

    Adding models

---
+++ Notificator.API/Models/Email.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Notificator.API.Models
+{
+    public class Email
+    {
+        [Required(ErrorMessage = "Required email address")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Address { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Required subject")]
+        [StringLength(40, ErrorMessage = "Subject length must be inferior than 40")]
+        public string Subject { get; set; } = string.Empty;
+
+        [StringLength(100, ErrorMessage = "String length must be inferior than 100")]
+        public string? Body { get; set; }
+    }
+}
---
+++ Notificator.API/Models/TelegramMessage.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Notificator.API.Models
+{
+    public class TelegramMessage
+    {
+        [Required(ErrorMessage = "Required username")]
+        [StringLength(32, MinimumLength = 5, ErrorMessage = "Username length must be between 5 and 32")]
+        [RegularExpression("^[a-zA-Z0-9_]*$", ErrorMessage = "Username must be conformed by a-z 0-9 and _")]
+        public string username { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Required message text")]
+        [StringLength(100, ErrorMessage = "Text length must be inferior than 100")]
+        public string text { get; set; } = string.Empty;
+    }
+}
