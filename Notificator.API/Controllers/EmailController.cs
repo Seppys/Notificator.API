325927c632211afd09a96f9751c4d85771806132
Jesus Ramirez <jesusmanuelr018@gmail.com>
2023-10-11 06:01:22 +0200

    Adding controllers

---
+++ Notificator.API/Controllers/EmailController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using Notificator.API.Models;
+using MailKit.Net.Smtp;
+
+namespace Notificator.API.Controllers
+{
+    [Route("api/email")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+       
+       [HttpPost("send")]
+       public IActionResult sendEmail(Email email)
+        {
+            var message = createMessage(email.Address, email.Subject, email.Body);
+
+            using (var smtp = new SmtpClient())
+            {
+                smtp.Connect("smtp.gmail.com", 587);
+                smtp.Authenticate(Environment.GetEnvironmentVariable("GMAIL_ADDRESS"), Environment.GetEnvironmentVariable("GMAIL_TOKEN"));
+
+                if (!smtp.IsConnected || !smtp.IsAuthenticated)
+                    return StatusCode(500);
+
+                smtp.Send(message);
+
+                smtp.Disconnect(true);
+
+                return Ok();
+            }
+        }
+
+        private MimeMessage createMessage(string address, string subject, string? body)
+        {
+            MimeMessage message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Notificator API", Environment.GetEnvironmentVariable("GMAIL_ADDRESS")));
+            message.To.Add(new MailboxAddress(null, address));
+            message.Subject = subject;
+            message.Body = new TextPart("plain") { Text = body };
+
+            return message;
+        }
+    }
+}
---
+++ Notificator.API/Controllers/TelegramController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Notificator.API.Models;
+using System.Text;
+
+namespace Notificator.API.Controllers
+{
+    [Route("api/telegram")]
+    [ApiController]
+    public class TelegramController : ControllerBase
+    {
+
+        private readonly IHttpClientFactory _httpClientFactory;
+        private string URL_BASE =
+            $"https://api.telegram.org/bot{Environment.GetEnvironmentVariable("TELEGRAM_BOT_TOKEN")}/";
+
+        public TelegramController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet("useradded/{username}")]
+        public async Task<IActionResult> IsUserRegistered(string username)
+        {
+            if (await IsValidTelegramToken() == false)
+                return StatusCode(500, new { error = "Invalid telegram token" });
+
+            bool isAdded = false;
+            string? chatId = await GetChatIdFromUsername(username);
+
+            if (chatId != null)
+                isAdded = true;
+
+            return Ok(new { isAdded });
+        }
+
+        [HttpPost("send")]
+        public async Task<IActionResult> sendMessage(TelegramMessage telegramMessage)
+        {
+            if (await IsValidTelegramToken() == false)
+                return StatusCode(500, new { error = "Invalid telegram token"});
+
+            string? chatId = await GetChatIdFromUsername(telegramMessage.username);
+
+            if (chatId == null)
+                return BadRequest(new { error = "Telegram chat not found" });
+
+            using StringContent jsonContent = new(
+                JsonConvert.SerializeObject(new
+                {
+                    chat_id = chatId,
+                    text = telegramMessage.text,
+                }),
+                Encoding.UTF8,
+                "application/json");
+
+            using (var client = _httpClientFactory.CreateClient())
+            {
+                HttpResponseMessage response = await client.PostAsync(
+                    URL_BASE + "sendMessage", jsonContent);
+            }
+
+            return Ok();
+        }
+
+        private async Task<string?> GetChatIdFromUsername(string username)
+        {
+            string? chatId = null;
+
+            using (var client = _httpClientFactory.CreateClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(URL_BASE + "getUpdates");
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    return null;
+                }
+
+                dynamic? jsonObj = JsonConvert.DeserializeObject(await response.Content.ReadAsStringAsync());
+
+                foreach (var update in jsonObj["result"])
+                {
+                    if (update["message"]["chat"]["username"] == username)
+                    {
+                        chatId = update["message"]["chat"]["id"];
+                        break;
+                    }
+                }
+
+                return chatId;
+            }
+        }
+
+        private async Task<Boolean> IsValidTelegramToken()
+        {
+            using (var client = _httpClientFactory.CreateClient())
+            {
+                var response = await client.GetAsync(URL_BASE + "getMe");
+                if (response.IsSuccessStatusCode)
+                    return true;
+
+                return false;
+            }
+        }
+    }
+}
