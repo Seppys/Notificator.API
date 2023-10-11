54b8da06aa3933f3b23325175a67f257da262fcd
Jesus Ramirez <jesusmanuelr018@gmail.com>
2023-10-11 06:02:18 +0200

    Adding documentation

--- Notificator.API/Program.cs
+++ Notificator.API/Program.cs
@@ -1,4 +1,5 @@
 using dotenv.net;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,9 +8,20 @@ DotEnv.Load();
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Notificator API",
+        Description = "<h2>Description</h2>" +
+            "<p>An ASP.Net API for sending Telegram messages and emails.</p>" +
+            "<h2>Requisites</h2>" +
+            "<p>For using Telegram services you need to start a chat with the " +
+            $"<a href=\"{Environment.GetEnvironmentVariable("TELEGRAM_BOT_URL")}\">Telegram bot</a>.</p>",
+    });
+});
 
 builder.Services.AddHttpClient();
 
